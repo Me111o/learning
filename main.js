7b3c28e89fbeff43d16f9b44a223de473d41d6b8
Me111o <81253949+Me111o@users.noreply.github.com>
2021-03-24 18:54:08 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const mySwiper = new Swiper('.swiper-container', {
 
 const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
-const modalClose = document.querySelector('.modal-close')
+
 
 const openModal = function() {
 	modalCart.classList.add('show')
@@ -25,20 +25,91 @@ const closeModal =function(){
 };
 
 buttonCart.addEventListener('click',openModal);
-modalClose.addEventListener('click',closeModal);
+modalCart.addEventListener('click', function(event){
+	const target= event.target;
+	if(target.classList.contains('overlay')|| target.classList.contains('modal-close')) {
+		closeModal()
+	}
+	
+})
 
 //scroll smooth
 
 const scrollLinks = document.querySelectorAll('a.scroll-link');
 
 
-for(let i=0; i < scrollLinks.length; i++){
-	scrollLinks[i].addEventListener('click', function(event){
+for(const scrollLink of scrollLinks){
+	scrollLink.addEventListener('click', function(event){
 		event.preventDefault();
-		const id = scrollLinks[i].getAttribute('href');
+		const id = scrollLink.getAttribute('href');
 		document.querySelector(id).scrollIntoView({
 			behavior: 'smooth',
 			block:'start'
 		})
 	});
 }
+
+
+//goods
+const more = document.querySelector('.more');
+const navigationLink = document.querySelectorAll('.navigation-link');
+const longGoodsList=document.querySelector('.long-goods-list');
+
+const getGoods = async function (){
+	const result = await fetch ('db/db.json');
+	if(!result.ok){
+		throw 'Error:' + result.status
+	}
+	return result.json();
+};
+getGoods().then(function(data){
+	console.log(data)
+});
+
+const createCard = function({label,name,img,description,id,price}){
+	const card = document.createElement('div');
+	card.className='col-lg-3 col-sm-6'
+
+	card.innerHTML = `
+	<div class="goods-card">
+	${label ? `<span class="label">${label}</span>`:''}
+			<img src='db/${img}' alt="${name}" class="goods-image">
+			<h3 class="goods-title">${name}</h3>
+			<p class="goods-description">${description}</p>
+			<button class="button goods-card-btn add-to-cart" data-id='${id}'>
+			<span class="button-price">$${price}</span>
+			</button>
+	</div>
+	`;
+
+return card;
+}
+
+const renderCards = function (data) {
+	longGoodsList.textContent='';
+	const cards = data.map(createCard)
+	longGoodsList.append(...cards)
+	document.body.classList.add('show-goods')
+};
+more.addEventListener('click', function(event){
+	event.preventDefault();
+	getGoods().then(renderCards);
+} );
+
+const filterCards = function (field,value){
+	getGoods().then(function(data){
+		const filteredGoods = data.filter(function(good){
+		return	good[field]===value
+		});
+		return filteredGoods;
+	})
+	.then(renderCards);
+}
+navigationLink.forEach(function(link){
+	link.addEventListener('click', function(event){
+		event.preventDefault();
+		const field = link.dataset.field;
+		const value =link.textContent;
+		filterCards(field,value)
+	})
+})
